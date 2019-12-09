c94251c7bb63b19beb543339d1334f13529afa30
Alex Dollard <alexdollard141@gmail.com>
2019-12-09 12:21:02 -0600

    got anime info appearing in first card

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-$("#yearButton").on("click", function(event){
-    event.preventDefault();
-    searchAnimeYear();
-});
+// $("#yearButton").on("click", function(event){
+//     event.preventDefault();
+//     searchAnimeYear();
+// });
 
 // returns all animes from 1-setNumber made in a given year
 function searchAnimeYear()
 {
-
+let index = 1
 for (let i=1; i<500; i++)
     {
 
 $.ajax({
-    url: "" + [i],
+    url: "https://kitsu.io/api/edge/anime/" + [i],
     method: "GET",
        })
 .then(function(response)
         {
 // console.log(response);
+// console.log(response.data.attributes.startDate.split("-")[0])
            let year = JSON.parse(localStorage.getItem("year"))
-            if(response.data.attributes.startDate.split("-")[0] === 2001){
+            if(response.data.attributes.startDate.split("-")[0] === year){
      
-    // console.log(response.data.attributes.canonicalTitle);
-    // console.log(response.data.attributes.startDate.split("-")[0]);
-    // console.log(response.data.attributes.posterImage.original);
-    // console.log(response.data.attributes.averageRating);
-    // console.log(response.data.attributes.synopsis);
+    console.log(response.data.attributes.canonicalTitle);
+    console.log(response.data.attributes.startDate.split("-")[0]);
+    console.log(response.data.attributes.posterImage.original);
+    console.log(response.data.attributes.averageRating);
+    console.log(response.data.attributes.synopsis);
 
     let animeName = response.data.attributes.canonicalTitle;
     let animeYear = response.data.attributes.startDate.split("-")[0];
@@ -32,31 +33,37 @@ $.ajax({
     let animeRating = response.data.attributes.averageRating; 
     let animeSynopsis = response.data.attributes.synopsis; 
 
-    let newDiv = $("<div>");
-                newDiv.attr("class", "anime_div")
-    let newImg = $("<img>");
-                newImg.attr("src", animePoster);
-                newImg.attr("height", 200);
-                newImg.attr("width", 120);
-                newImg.attr("alt", "Poster");
-    let name = $("<p>").text("Title: " + animeName);
-    let year = $("<p>").text("Year: " + animeYear);
-    let synopsis = $("<p>").text("Synopsis: " + animeSynopsis);
-    let rating = $("<p>").text("Average Rating: " + animeRating);
+    // let newDiv = $("<div>");
+    //             newDiv.attr("class", "anime_div")
+    // let newImg = $("<img>");
+    //             newImg.attr("src", animePoster);
+    //             newImg.attr("height", 200);
+    //             newImg.attr("width", 120);
+    //             newImg.attr("alt", "Poster");
+    // let name = $("<p>").text("Title: " + animeName);
+    // let year = $("<p>").text("Year: " + animeYear);
+    // let synopsis = $("<p>").text("Synopsis: " + animeSynopsis);
+    // let rating = $("<p>").text("Average Rating: " + animeRating);
 
-    $("body").append(newDiv);
-    newImg.appendTo(newDiv);
-    name.appendTo(newDiv);
-    rating.appendTo(newDiv);
-    synopsis.appendTo(newDiv); 
+    // $("body").append(newDiv);
+    // newImg.appendTo(newDiv);
+    // name.appendTo(newDiv);
+    // rating.appendTo(newDiv);
+    // synopsis.appendTo(newDiv); 
+
+    $("#card_" + index + "_title").text(animeName);
+    $("#card_" + index + "_rating").text(animeRating);
+    $("#card_" + index + "_synopsis").text(animeYear);
+    $("#card_" + index + "_poster").css("background", "url(" + animePoster + ")")
 
 
+    index++;
             }
             
         })
     }
 }
-// searchAnimeYear(); 
+searchAnimeYear(); 
 
 
 
@@ -84,29 +91,29 @@ $.ajax({
 
 
 
-// returns the GENRE DATA of animes from 1-setNumber
-function searchGenres()
-{
+// // returns the GENRE DATA of animes from 1-setNumber
+// function searchGenres()
+// {
 
-    for(let i=1; i<10; i++ )
-    {
-        $.ajax({
-            url: "https://kitsu.io/api/edge/anime/" + [i] + "/genres",
-            method: "GET"
-        })
-        .then(function(response)
-            {
-            let array = response.data
-            for (i=0; i<array.length; i++){
-                console.log(array[i].attributes.name);
-                console.log(i);
+//     for(let i=1; i<10; i++ )
+//     {
+//         $.ajax({
+//             url: "https://kitsu.io/api/edge/anime/" + [i] + "/genres",
+//             method: "GET"
+//         })
+//         .then(function(response)
+//             {
+//             let array = response.data
+//             for (i=0; i<array.length; i++){
+//                 console.log(array[i].attributes.name);
+//                 console.log(i);
     
-            }
+//             }
         
-            })
-    }
-}
-// searchGenres();
+//             })
+//     }
+// }
+// // searchGenres();
 
 
 
@@ -142,7 +149,13 @@ function searchGenres()
             localStorage.setItem("year", JSON.stringify(movieYear));
             
             $("#movie-title").text(movieTitle);
+            $("#movie-year").text(movieYear);
+            $("#movie-genre").text(movieGenre);
+            $("#movie-synopsis").text(moviePlot);
+            
+
         })
+        searchAnimeYear();
     });
         
 
