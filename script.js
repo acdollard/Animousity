9522674e65f8ce61f7e03627f95bf4b72917a940
Tyler <tgandle@gmail.com>
2019-12-09 17:23:43 -0600

    edited some stuff, but no major files were changed

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ for (let i=1; i<500; i++)
     {
 
 $.ajax({
-    url: "" + [i],
+    url: "https://kitsu.io/api/edge/anime/?filter[startDate]" + [i],
     method: "GET",
        })
 .then(function(response)
