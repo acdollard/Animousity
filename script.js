7a734b9373abf73306557c09b28db2ba2330f987
Alex Dollard <alexdollard141@gmail.com>
2019-12-12 14:34:13 -0600

    Added a simple vue.js to say what time the page was rendered

--- script.js
+++ script.js
@@ -162,3 +162,9 @@ $.ajax({
 
         
 
+    var app2 = new Vue({
+        el: '#app',
+        data: {
+          message: 'You loaded this page on ' + new Date().toLocaleString()
+        }
+      })
