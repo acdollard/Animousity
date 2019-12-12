e83c28d67fec4d3a0c469fe610a693aa72354df6
Alex Dollard <alexdollard141@gmail.com>
2019-12-11 20:38:24 -0600

    added buttons for year and genre function, got them working

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 
+    $("#yearBtn").on("click", function(event){
+        event.preventDefault();
+        searchAnimeYear();
+    });
+
+    $("#genreBtn").on("click", function(event){
+        event.preventDefault();
+        searchAnimeGenre();
+    });
+
+
+
+
+
+
 function searchAnimeYear()
 {
 let index = 1
@@ -36,7 +51,7 @@ $.ajax({
     $("#card_" + index + "_title").text(animeName);
     $("#card_" + index + "_rating").text("Average Rating: " + animeRating);
     $("#card_" + index + "_synopsis").text(animeSynopsis);
-    $("#card_" + index + "_synopsis").css("font-size", "10px");
+    $("#card_" + index + "_synopsis").css("font-size", "9px");
     $("#card_" + index + "_poster").css("background", "url(" + animePoster + ")")
 
 
@@ -52,23 +67,48 @@ $.ajax({
 
 
 // // returns all anime of a given genre *needs work
-// function searchAnimeGenre()
-// {
+function searchAnimeGenre()
+{
+let getGenre = JSON.parse(localStorage.getItem("genre"));
+$.ajax({
+    url: "https://kitsu.io/api/edge/anime/?filter[genres]=" + getGenre,
+    // need sort??
+    method: "GET",
+       })
+.then(function(response)
+        {
+            let counter = 0;
+            for(i=0; i<10; i++)
+            {
+console.log(response);
+console.log(response.data[i].attributes.canonicalTitle);
+console.log(response.data[i].attributes.startDate.split("-")[0]);
+console.log(response.data[i].attributes.posterImage.original);
+console.log(response.data[i].attributes.averageRating);
+console.log(response.data[i].attributes.synopsis);
 
-// $.ajax({
-//     url: "https://kitsu.io/api/edge/anime/?filter[genres]=action,comedy,sci-fi",
-//     // need sort??
-//     method: "GET",
-//        })
-// .then(function(response)
-//         {
- 
-// console.log(response);
+let animeName = response.data[i].attributes.canonicalTitle;
+let animeYear = response.data[i].attributes.startDate.split("-")[0];
+let animePoster = response.data[i].attributes.posterImage.original;
+// let animeRating = response.data.attributes.averageRating; 
+let animeSynopsis = response.data[i].attributes.synopsis; 
 
-//         })
+
+$("#card_" + counter + "_title").text(animeName);
+$("#card_" + counter + "_rating").text("Year: " + animeYear);
+$("#card_" + counter + "_synopsis").text(animeSynopsis);
+$("#card_" + counter + "_synopsis").css("font-size", "9px");
+$("#card_" + counter + "_poster").css("background", "url(" + animePoster + ")");
+
+counter++;
+               }
+
+
+        })
     
-// }
-// // searchAnimeGenre(); 
+    
+}
+// searchAnimeGenre(); 
 
 
 
@@ -128,17 +168,19 @@ $.ajax({
             // console.log(response.Genre.split(" ")[0].replace(",",""));
             let movieTitle = $("<p>").text( "Title: " + response.Title)
             let movieGenre = $("<p>").text( "Genre: " + response.Genre.split(" ")[0].replace(",",""));
+            let local_storage_genre = response.Genre.split(" ")[0].replace(",","");
             let movieYear = $("<p>").text( "Released: " + response.Year);
             let local_storage_year = response.Year
             let moviePlot = $("<p>").text( "Plot: " + response.Plot);
             let moviePoster = $("<img>").attr("src", response.Poster);
             // console.log(response.Year);
-            localStorage.setItem("genre", JSON.stringify(movieGenre));
+            localStorage.setItem("genre", JSON.stringify(local_storage_genre));
             localStorage.setItem("year", JSON.stringify(local_storage_year));
             localStorage.setItem("plot", JSON.stringify(moviePlot));
             localStorage.setItem("title", JSON.stringify(movieTitle));
             localStorage.setItem("poster", JSON.stringify(moviePoster));
             
+            console.log(local_storage_genre);
 
             $("#movie-title").empty();
             $("#movie-title").append(movieTitle, movieGenre, movieYear, moviePlot);
@@ -151,12 +193,14 @@ $.ajax({
             $("#movie-synopsis").html(moviePlot);
 
             $("#posterHolder").attr("src", response.Poster);
+            // $("#posterHolder").height(300);
+            // $("#posterHolder").width(auto);
 
 
             
 
         })
-        searchAnimeYear();
+        // searchAnimeYear();
     });
 
 
